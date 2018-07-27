c1f103742faf752597bb123d92cc549607d2b385
Zuodion <zuodion@gmail.com>
2018-07-27 19:33:12 +0300

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -11,26 +11,40 @@ socket.on('disconnect', () => {//тригерится на отключение 
 
 socket.on('newMessage', (message) => {
     let formattedTime = moment(message.createdAt).format('H:mm');//отсылает время
-    console.log('New message', message);//клиент посылает на сервер сообщение, а ниже jQuery обрабатывает и помещает его
-    let li = jQuery('<li></li>');//li - новая форма
-    li.text(`${formattedTime} ${message.from}: ${message.text}`);// собственно придание li формы от кого, и сам текст
-    jQuery('#messages').append(li);//добавление в список сообщений с id(номером) который берется из index.html<ol>
+    let template = jQuery('#message-template').html();//вытягивает шаблон из index.html
+    let html = Mustache.render(template, {//рендер - создает следующее сообщение по шаблону template
+        createdAt: formattedTime,
+        from: message.from,
+        text: message.text
+    });
+
+    jQuery('#messages').append(html);//добавляет выше собраное сообщение
+
+//     let li = jQuery('<li></li>');//li - новая форма
+//     li.text(`${formattedTime} ${message.from}: ${message.text}`);// собственно придание li формы от кого, и сам текст
+//     jQuery('#messages').append(li);//добавление в список сообщений с id(номером) который берется из index.html<ol>
 });
 
 socket.on('newLocationMessage', (message) => {
     let formattedTime = moment(message.createdAt).format('H:mm');//отсылает время
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">Я здесь</a>');//blank - с нового окна
-    li.text(`${formattedTime} ${message.from}: `);
-    a.attr('href', message.url);//attr Получает/устанавливает значение атрибутов выбранных элементов(кароче заменяет href на message.url)
-    li.append(a);
-    jQuery('#messages').append(li);
+    let template = jQuery('#location-message-template').html();//вытягивает шаблон из index.html
+    let html = Mustache.render(template, {
+        createdAt: formattedTime,
+        from: message.from,
+        url: message.url
+    });
+    jQuery('#messages').append(html);
+    // let li = jQuery('<li></li>');
+    // let a = jQuery('<a target="_blank">Я здесь</a>');//blank - с нового окна
+    // li.text(`${formattedTime} ${message.from}: `);
+    // a.attr('href', message.url);//attr Получает/устанавливает значение атрибутов выбранных элементов(кароче заменяет href на message.url)
+    // li.append(a);
 });
 
 jQuery('#message-form').on('submit', (e) => {//событие submit и функция, которая выполняется после submit
     e.preventDefault();//кароче делает так чтобы в командной строке не образовывалось сообщение которое мы сабмитнули
 
-    let messageTextbox = jQuery('[name=message]')
+    let messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {//отправка от сервера к клиенту
         from: 'User',
