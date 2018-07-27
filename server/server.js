2acb6c8a413b5c515de3d7e0d499a1f94eee80dd
Zuodion <zuodion@gmail.com>
2018-07-27 13:03:24 +0300

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const socketIO = require('socket.io');
 const http = require('http');
 
+const {generateMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 let server = http.createServer(app);
@@ -14,25 +15,13 @@ let io = socketIO(server);
 io.on('connection', (socket)=> {//позволяет делать операции с ивентами
     console.log('New user connected');//например тригерить на нового юзера
 
-        socket.emit('newMessage', {
-            from: 'Admin',
-            text: 'Welcome to the chat app',
-            createdAt: new Date().getTime
-        });
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-        socket.broadcast.emit('newMessage', {//при заходе на сайт отсылает всем пользователям КРОМЕ зашедшего сообщение
-            from: 'Admin',
-            text: 'New user joined',
-            createdAt: new Date().getTime
-        });
+        socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
 
     socket.on('createMessage', (message) => {//создать и послать письмо - от клиента к серверу
         console.log('createMessage', message);
-        io.emit('newMessage', {//сообщение от сервера к клиенту
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
     //     socket.broadcast.emit('newMessage', {
     //         from: message.from,
